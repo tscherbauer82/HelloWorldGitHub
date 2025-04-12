16e094378f3ad1dfc759056ec5bcd2cd9107ed28
TSche <TSche@FAMILY-PC>
2025-04-12 08:19:28 -0400

    Add project files.

---
+++ HelloWorldGitHub.cpp
@@ -0,0 +1,12 @@
+//Tommy Scherbauer
+//CIS 1202.800
+//April 12, 2025
+//GitHub Test Project
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!\n";
+    return 0;
+}
